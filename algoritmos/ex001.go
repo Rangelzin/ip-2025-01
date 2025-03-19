720530cc3151eb5f6cdb60a64c743a54c1480724
Rangelzin <victorrangeltasse85@hotmail.com>
2025-03-19 20:41:31 -0300

    Resolução ex002.go

---
+++ algoritmos/ex001.go
@@ -0,0 +1,25 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var media float32
+	notas := [3]float32{1,2,3}
+	
+
+	fmt.Println("Envie as 3 notas: ")
+	fmt.Scanln(&notas[0],&notas[1],&notas[2])
+
+	media = (notas[0]+ notas[1]+ notas[2]) / 3.0
+
+	if media >= 6 {
+		fmt.Println(fmt.Sprintf("A Média do aluno é: "),media)
+		fmt.Println("APROVADO!")
+	} else {
+		fmt.Println(fmt.Sprintf("A Média do aluno é: "),media)
+		fmt.Println("REPROVADO!")
+	}
+}
