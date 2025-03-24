afe6f78f586d48795ec9b52b4875ff5048e929f6
Rangelzin <victorrangeltasse85@hotmai.com>
2025-03-24 14:54:12 -0300

    Resolução completa do ex006.go

---
+++ algoritmos/ex006.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	var qtd int
+	tempF :=  [3]float32{1,2,3}
+	tempC :=  [3]float32{1,2,3}
+	
+	fmt.Print("Quantas temperaturas deseja informar? ")
+	fmt.Scan(&qtd)
+
+	for i := 0; i < qtd; i++ {
+		fmt.Print("Qual a temperatura Nº", i+1, ": ")
+		fmt.Scan(&tempF[i])
+		
+		tempC[i] = 5 * (tempF[i] - 32)/9
+	}
+	for i := 0; i < qtd; i++ {
+		fmt.Printf("%.2f Fahrenheit equivale a %.2f Celsius\n", tempF[i], tempC[i])
+	}
+}
