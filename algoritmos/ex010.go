c93bb4c0b0e74a1566d2511d9880478530234c69
Rangelzin <victorrangeltasse85@hotmai.com>
2025-03-25 15:07:50 -0300

    Resolução completa do ex010.go

---
+++ algoritmos/ex010.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a,b,c,d,det float32
+
+	fmt.Println("Digite o valor dos quatro elementos da matriz: ")
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&d)
+
+
+	det = a*d - b*c
+	fmt.Printf("O valor do determinante é = %.2f\n", det)
+}
