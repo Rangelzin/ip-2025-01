9e111d220940ba75fc8944c6db5ea6ccf4f02fbe
Rangelzin <victorrangeltasse85@hotmai.com>
2025-03-25 15:08:11 -0300

    Resolução completa do ex012.go

---
+++ algoritmos/ex012.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+
+func main() {
+
+	var horas,valor float64
+
+	fmt.Println("Quantidade de horas que a charrete foi usada: ")
+	fmt.Scan(&horas)
+
+	if horas <= 3 {
+		valor = horas * 5
+	} 
+
+	if horas > 3 {
+		valor = math.Floor(float64(horas/3)) * 10 + (horas - (math.Floor(float64(horas/3)) * 3)) * 5
+	}
+
+	
+
+	fmt.Printf("O valor a ser pago é de R$ %.2f\n", valor)
+}
