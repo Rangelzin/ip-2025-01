eb7502c7e7c0b80b6d747af86db265e0d84749e9
Rangelzin <victorrangeltase85@hotmail.com>
2025-03-26 14:40:09 -0300

    Resolução completa do ex016.go

---
+++ algoritmos/ex016.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var (
+		salary,newSalary float32
+	)
+
+	fmt.Print("Digite o valor o salário: R$")
+	fmt.Scan(&salary)
+
+	if salary <= 300 {
+		newSalary =  salary * 1.5
+	} else {
+		newSalary =  salary * 1.3
+	}
+
+	fmt.Printf("Salário com reajuste = R$%.2f\n", newSalary)
+
+}
