5b0bcbb582794f586ae47427a2e13ded96c1521b
Rangelzin <victorrangeltase85@hotmail.com>
2025-03-26 17:18:08 -0300

    Resolução completa do ex018.go

--- algoritmos/ex018.go
+++ algoritmos/ex018.go
@@ -7,15 +7,14 @@ import (
 func main() {
 
 	var (
-		valInit, r , n int
+		valInit, r , n,soma int
 	)
 
 	fmt.Print("Digite o valor inicial / razão / qtd de elementos: ")
 	fmt.Scan(&valInit, &r, &n)
 
-	valEnd := valInit + ((n-1)*r)
-
-	soma := ((valInit + valEnd)*n) / 2 
-
+	for i := 1; i <= n; i++ {
+		soma += valInit + ((i-1)*r)
+	}
 	fmt.Printf("A soma da P.A é %d\n", soma)
 }
