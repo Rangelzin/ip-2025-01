daed581723e33337c1a5e16b293c7d6aa677e169
Rangelzin <victorrangeltasse85@hotmail.com>
2025-04-04 18:51:37 -0300

    Novos Exec da LISTA02

--- lista02/ex015.go
+++ lista02/ex015.go
@@ -9,16 +9,19 @@ func main() {
 	fmt.Print("Digite a data que queria formatar (EX: X XX XXXX): ")
 	fmt.Scan(&dia, &mes, &ano)
 
-if mes == 1 { fmt.Printf("%d de Janeiro de %d", dia, ano)}
-if mes == 2 { fmt.Printf("%d de Fevereiro de %d", dia, ano)}
-if mes == 3 { fmt.Printf("%d de Março de %d", dia, ano)}
-if mes == 4 { fmt.Printf("%d de Abril de %d", dia, ano)}
-if mes == 5 { fmt.Printf("%d de Maio de %d", dia, ano)}
-if mes == 6 { fmt.Printf("%d de Junho de %d", dia, ano)}
-if mes == 7 { fmt.Printf("%d de Julho de %d", dia, ano)}
-if mes == 8 { fmt.Printf("%d de Agosto de %d", dia, ano)}
-if mes == 9 { fmt.Printf("%d de Setembro de %d", dia, ano)}
-if mes == 10 { fmt.Printf("%d de Outubro de %d", dia, ano)}
-if mes == 11 { fmt.Printf("%d de Novembro de %d", dia, ano)}
-if mes == 12 { fmt.Printf("%d de Dezembro de %d", dia, ano)}
+	switch mes {
+		case 1: fmt.Printf("%d de Janeiro de %d", dia, ano)
+		case 2: fmt.Printf("%d de Fevereiro de %d", dia, ano)
+		case 3: fmt.Printf("%d de Março de %d", dia, ano)
+		case 4: fmt.Printf("%d de Abril de %d", dia, ano)
+		case 5: fmt.Printf("%d de Maio de %d", dia, ano)
+		case 6: fmt.Printf("%d de Junho de %d", dia, ano)
+		case 7: fmt.Printf("%d de Julho de %d", dia, ano)
+		case 8: fmt.Printf("%d de Agosto de %d", dia, ano)
+		case 9: fmt.Printf("%d de Setembro de %d", dia, ano)
+		case 10: fmt.Printf("%d de Outubro de %d", dia, ano)
+		case 11: fmt.Printf("%d de Novembro de %d", dia, ano)
+		case 12: fmt.Printf("%d de Dezembro de %d", dia, ano)
+	}
+
 }
